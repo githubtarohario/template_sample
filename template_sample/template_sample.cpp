a403b7dd22c580393a77d37be6da5a2cb8ce25eb
PCUSER <taka_ken2000@yahoo.co.jp>
2024-11-26 19:41:06 +0900

    プロジェクト ファイルを追加します。

---
+++ template_sample/template_sample.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+// 関数テンプレートの定義
+template <typename T>
+T maxValue(T a, T b) {
+    return (a > b) ? a : b;
+}
+/*
+int Max_(int x, int y) {
+    return (x > y) ? x : y;
+
+
+}
+*/
+
+
+
+
+int main() {
+    int intA = 10;
+    int intB = 20;
+    cout << "Max (int): " << maxValue(intA, intB) << endl; // int型で呼び出し
+
+    double doubleA = 5.5;
+    double doubleB = 3.3;
+    cout << "Max (double): " << maxValue(doubleA, doubleB) << endl; // double型で呼び出し
+
+    char charA = 'a';
+    char charB = 'z';
+    cout << "Max (char): " << maxValue(charA, charB) << endl; // char型で呼び出し
+
+    return 0;
+}
